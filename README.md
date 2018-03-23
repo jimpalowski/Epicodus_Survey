b9615c48a64798c8baaf9466e85086e88ad1bd12
Jim Palowski <palowskijim@gmail.com>
2018-03-23 16:43:25 -0700

    Add more complex answers to narrow down a search and finished business/user logic

---
+++ README.md
@@ -0,0 +1,18 @@
+#Author
+
+Jim Palowski
+
+#Epicodus Survey
+_{A simple survey}_
+#Setup
+
+
+_{Pick an answer for every box to get a result}_
+
+## Description
+
+_{This project is for a survey to be completed by a person who is unsure of what programming track to take}_
+
+#Copyright
+
+*Copyright (c) 2018 **_{Jim Palowski}_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,119 +1,251 @@
 //BUSINESS LOGIC
 
-//my function will take a genre, an age group and a season and will return a celebrity match
 function EpicodusSurvey(genres, location, time, role, idea) {
-    var result1 = "python";
-    var result2 = "ruby";
-    var result3 = "c++";
+    var result1 = "python2";
+    var result2 = "ruby2";
+    var result3 = "cplus";
     var result4 = "csharp";
-    var result6 = "java";
-    var result7 = "javascript";
-    var result8 = "php";
-    var result9 = "swift";
-  
-  if (genres == 1) { //FOR COMEDY LOVERS
-    if (location == "5") {
-        if(time == "easy")
+    var result5 = "java";
+    var result6 = "javascript";
+    var result7 = "php";
+    var result8 = "swift";
+    var result9 = "NONE";
+
+  if (genres == "Complex") {
+    if (location == "CA") {
+        if(time == "Weekend" || "M-F")
             if(role == "begin")
-                if(idea == "front")
+                if(idea="front" || "back")
         {
           return result1;
-        } else {
-          return result2;
         }
-    } else {
-      if(location == "6")
-      {
-        return result3;
-      } else {
-        return result4;
+       }    
       }
-    }
-  }
-  
-  if (genres == 2) { //FOR COMEDY LOVERS
-    if (location == "6") {
-        if(time == "easy")
-            if(role == "inter")
-                if(idea == "back")
-        {
-          return result5;
-        } else {
-          return result6;
-        }
-    } else {
-      if(location == "micro")
-      {
-        return result7;
-      } else {
-        return result8;
-      }
-    }
-  }
+    if (genres == "Hard") {
+        if (location == "WA") {
+            if(time == "24-7")
+                if(role == "advanced")
+                    if(idea="full")
+            {
+                return result4;
+            }
+            }    
+            }
+
+    if (genres == "Universal") { 
+        if (location == "NY") {
+            if(time == "M-F")
+                if(role == "intermediate")
+                    if(idea="back")
+            {
+                return result5;
+            }
+            }    
+            }
+
+    if (genres == "Complex") { 
+        if (location == "WA") {
+            if(time == "24-7")
+                if(role == "advanced")
+                    if(idea="full")
+            {
+                return result3;
+            }
+            }    
+            }
+
+    if (genres == "Hard") { 
+        if (location == "CA") {
+            if(time == "Weekend" || "M-F")
+                if(role == "intermediate")
+                    if(idea="front")
+            {
+                return result7;
+            }
+            }    
+            }
+    if (genres == "Hard") { 
+        if (location == "CA") {
+            if(time == "Weekend" || "M-F")
+                if(role == "intermediate")
+                    if(idea="front")
+            {
+                return result7;
+            }
+            }    
+            }
+
+    if (genres == "Hard") { 
+        if (location == "NY") {
+            if(time == "Weekend" || "24-7")
+                if(role == "advanced")
+                    if(idea="full")
+            {
+                return result8;
+            }
+            }    
+            }
+    if (genres == "Complex") { 
+        if (location == "NY") {
+            if(time == "Weekend" || "24-7")
+                if(role == "begin")
+                    if(idea="front")
+            {
+                return result5;
+            }
+            }    
+            }
+                    
+    if (genres == "Hard") { 
+            if (location == "NY") {
+                if(time == "Weekend" || "24-7")
+                    if(role == "advanced")
+                        if(idea="full")
+                {
+                    return result8;
+                }
+                }    
+                }                   
+
+    if (genres == "Hard") { 
+        if (location == "NY") {
+            if(time == "M-F" || "Weekend")
+                if(role == "intermediate")
+                    if(idea="back")
+            {
+                return result6;
+            }
+            }    
+            }
+
+    if (genres == "Universal") { 
+        if (location == "CA") {
+            if(time == "M-F" || "24-7")
+                if(role == "intermediate")
+                    if(idea="full")
+            {
+                return result7;
+            }
+            }    
+            }
+
+    if (genres == "Hard") { 
+        if (location == "WA") {
+            if(time == "M-F" || "24-7")
+                if(role == "advanced")
+                    if(idea="back")
+            {
+                return result4;
+            }
+            }    
+            }
+
+    if (genres == "Universal") { 
+        if (location == "WA") {
+            if(time == "M-F" || "Weekend")
+                if(role == "begin")
+                    if(idea="front" || "back")
+            {
+                return result1;
+            }
+            }    
+            }
+
+
+    if (genres == "Hard") { 
+        if (location == "WA") {
+            if(time == "M-F" || "24-7")
+                if(role == "begin")
+                    if(idea="back" || "full")
+            {
+                return result3;
+            }
+            }    
+            }
 
 
 
-  if (genres == 3) { //FOR COMEDY LOVERS
-    if (location == "7") {
-        if(time == "micro")
-            if(role == "ex")
-                if(idea == "full")
-        {
-          return result9;
-        } else {
-          return result1;
-        }
-    } else {
-      if(location == "2")
-      {
-        return result3;
-      } else {
-        return result6;
-      }
-    }
-  }
+
+    if (genres == "Hard") { 
+        if (location == "WA") {
+            if(time == "M-F" || "24-7")
+                if(role == "advanced")
+                    if(idea="back")
+            {
+                return result4;
+            }
+            }    
+            }
+
+    if (genres == "Universal") { 
+        if (location == "NY") {
+            if(time == "Weekend")
+                if(role == "intermediate")
+                    if(idea="back")
+            {
+                return result2;
+            }
+            }    
+            }
+
+    if (genres == "Complex") { 
+        if (location == "NY") {
+            if(time == "24-7" || "M-F")
+                if(role == "advanced")
+                    if(idea="back" || "full")
+            {
+                return result6;
+            }
+            }    
+            }
+    if (genres == "Hard") { 
+        if (location == "WA") {
+            if(time == "M-F" || "24-7")
+                if(role == "intermediate")
+                    if(idea="front")
+            {
+                return result3;
+            }
+            }    
+            }
 
 
+    if (genres == "Universal") { 
+        if (location == "CA") {
+            if(time == "M-F" || "24-7")
+                if(role == "begin")
+                    if(idea="back")
+            {
+                return result2;
+            }
+            }    
+            }       
 
+    if (genres == "Universal") { 
+        if (location == "NY") {
+            if(time == "Weekend" || "M-F")
+                if(role == "begin")
+                    if(idea="back")
+            {
+                return result7;
+            }
+            }    
+            }
+    if (genres == "Hard") { 
+        if (location == "CA") {
+            if(time == "Weekend" || "M-F")
+                if(role == "advanced")
+                    if(idea="back")
+            {
+                return result8;
+            }
+            }    
+            }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    else{
+        return result9;
+ }
 }
-  
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -134,11 +266,10 @@ function EpicodusSurvey(genres, location, time, role, idea) {
   
       var userResult = EpicodusSurvey(userGenre, userLocation, userTime, userRole, userIdea);
   
-        $("#"+userResult).show(); //be showing the image of the chosen celebrity
+        $("#"+userResult).show(); //be showing the image of the chosen language
   
-  //var radioValue = $("input[name='Celebrity']:checked").val();
+
   
     });
   
   });
-  
